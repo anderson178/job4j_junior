afd6e3b6724bfec74451f757813cb553be83ffb1
anderson178 <denismironenko@yandex.ru>
2019-01-31 21:04:07 +0300

    5.1.1. Итератор для двухмерного массива int[][]

---
+++ chapter_001/src/main/java/ru/job4j/array/IterratorJaggedArray.java
@@ -0,0 +1,77 @@
+package ru.job4j.array;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 31.01.2019
+ */
+
+public class IterratorJaggedArray implements Iterator<Integer> {
+    int[][] array;
+    int carriage = 0;
+    int countElements = 0;
+
+    public IterratorJaggedArray(int[][] array) {
+        this.array = array;
+        counts();
+    }
+
+    /**
+     * Сounting the number of elements in the entire array
+     */
+    private void counts() {
+        for (int[] mas : this.array) {
+            countElements += mas.length;
+        }
+    }
+
+    /**
+     * Method for getting an element from an array
+     *
+     * @return - element an array
+     */
+    private Integer getElemnt() {
+        Integer rst = null;
+        int count = 0;
+        for (int i = 0; i < this.array.length; i++) {
+            for (int j = 0; j < this.array[i].length; j++) {
+                if (count == carriage) {
+                    return this.array[i][j];
+                } else {
+                    count++;
+                }
+            }
+        }
+        return rst;
+    }
+
+    /**
+     * Checks if the next element is
+     *
+     * @return - true or false
+     */
+    @Override
+    public boolean hasNext() {
+        return carriage < this.countElements;
+    }
+
+    /**
+     * takes the next element
+     *
+     * @return - element
+     * @throws NoSuchElementException - if no such element
+     */
+    @Override
+    public Integer next() throws NoSuchElementException {
+        if (this.getElemnt() != null) {
+            Integer rst = this.getElemnt();
+            carriage++;
+            return rst;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/IterratorJaggedArrayTest.java
@@ -0,0 +1,102 @@
+package ru.job4j.array;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 31.01.2019
+ */
+
+public class IterratorJaggedArrayTest {
+    private IterratorJaggedArray it;
+    private IterratorJaggedArray itTwo;
+
+    @Before
+    public void setUp() {
+        it = new IterratorJaggedArray(new int[][]{{1, 2, 3}, {4, 5, 6}});
+        itTwo = new IterratorJaggedArray(new int[][]{{1}, {3, 4}, {7}});
+    }
+
+    @Test
+    public void hasNextNextSequentialInvocation() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(6));
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(6));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shoulThrowNoSuchElementException() {
+        it = new IterratorJaggedArray(new int[][]{});
+        it.next();
+    }
+
+    @Test
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocationItTwo() {
+        assertThat(itTwo.next(), is(1));
+        assertThat(itTwo.next(), is(3));
+        assertThat(itTwo.next(), is(4));
+        assertThat(itTwo.next(), is(7));
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrderItTwo() {
+        assertThat(itTwo.hasNext(), is(true));
+        assertThat(itTwo.hasNext(), is(true));
+        assertThat(itTwo.next(), is(1));
+        assertThat(itTwo.next(), is(3));
+        assertThat(itTwo.next(), is(4));
+        assertThat(itTwo.next(), is(7));
+    }
+
+    @Test
+    public void hasNextNextSequentialInvocationItTwo() {
+        assertThat(itTwo.hasNext(), is(true));
+        assertThat(itTwo.next(), is(1));
+        assertThat(itTwo.hasNext(), is(true));
+        assertThat(itTwo.next(), is(3));
+        assertThat(itTwo.hasNext(), is(true));
+        assertThat(itTwo.next(), is(4));
+        assertThat(itTwo.hasNext(), is(true));
+        assertThat(itTwo.next(), is(7));
+        assertThat(itTwo.hasNext(), is(false));
+    }
+}
