a47f16a2de17558f0b2fc35431ed9d738a99bd33
anderson178 <denismironenko@yandex.ru>
2019-02-07 14:35:59 +0300

    5.1.4. Создать convert(Iterator<Iterator>) rename diractory

--- chapter_001/src/main/java/ru/job4j/iterator/IterratorJaggedArray.java
+++ chapter_001/src/main/java/ru/job4j/iterator/IterratorJaggedArray.java
@@ -1,4 +1,4 @@
-package ru.job4j.array;
+package ru.job4j.iterator;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
--- chapter_001/src/test/java/ru/job4j/iterator/IteratorEventNumbersTest.java
+++ chapter_001/src/test/java/ru/job4j/iterator/IteratorEventNumbersTest.java
@@ -1,4 +1,4 @@
-package ru.job4j.array;
+package ru.job4j.iterator;
 
 import org.junit.Before;
 import org.junit.Test;
