ba8bb86100b2dc55f26d3bbd69c625bb009582b7
anderson178 <denismironenko@yandex.ru>
2019-03-12 17:17:03 +0300

    Статистика по коллекции. [#45889]

--- chapter_001/src/main/java/ru/job4j/task/Analize.java
+++ chapter_001/src/main/java/ru/job4j/task/Analize.java
@@ -2,65 +2,85 @@ package ru.job4j.task;
 
 import java.util.*;
 
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 12.03.2019
+ */
+
+
 public class Analize {
-    private boolean check = false;
+    private Info info = new Info();
 
+    /**
+     * Метод определяет разницу между первым и вторым листом.
+     * Весь метод основывается на простом алгоритме метода исключений, когда убираем варианты которые не могут
+     * подвергаться исходному условия. В кадом подметоде каждый раз мы укорачиваем листы если это удовлетворяет
+     * условию, а остатки фиксируем в счетчиках info.
+     * Hashset для info.added необходимо что бы избавитьс от дубликатов
+     *
+     * @param previous - лист с изначальным состояние
+     * @param current  - лист с текущим состоянием
+     * @return - объект info содержащий подсчитанную разницу листов
+     */
     public Info diff(List<User> previous, List<User> current) {
-        Info info = new Info();
-        List<User> temp = new ArrayList<>(current);
-        List<User> temp2 = new ArrayList<>(current);
-
-        List<User> tempPrev = new ArrayList<>(previous);
-        List<User> tempCurt = new ArrayList<>(current);
-
-        for (int i = 0; i < tempPrev.size(); i++) {
-            this.check = false;
-            for(User userC : tempCurt) {
-                if (tempPrev.get(i).getId().equals(userC.getId())) {
-                    this.check = true;
-                    break;
-                }
-            }
-            if (!this.check) {
-                tempPrev.remove(i--);
-                info.deleted++;
-            }
-        }
-
-
-            for (User userP : previous) {
-                temp = new ArrayList<>(temp2);
-                for (User userC : temp) {
-                    if (userP.getId().equals(userC.getId()) && userP.getName().equals(userC.getName())) {
-                        temp2.remove(userC);
-                    }
-                    if (userP.getId().equals(userC.getId()) && !userP.getName().equals(userC.getName())) {
-                        temp2.remove(userC);
-                        info.cnanged++;
-                    }
-                }
-            }
-        info.added = temp2.size();
-        int p = 0;
-
-
+        List<User> tempPrev = this.infoDeleted(previous, current);
+        List<User> tempCurt = this.infoChanged(tempPrev, current);
+        info.added = new HashSet<>(tempCurt).size();
         return info;
     }
 
-    /*private void different(List<User> first, List<User> second, Info info) {
-        for (User userS : second) {
-            for (User userF : first) {
-                if(userS.getId().equals(userF.getId()) && userS.getName().equals(userF.getName())) {
-                    this.check = true;
+    /**
+     * Метод подсчета изменных объектов.
+     * Работает за счет того, что удаляет из временного листа совпадающие одинаковыые объекты и объекты поле name
+     * которых не совпадает (последнее фиксируется в счетчике изменений) с листом текущего состоения
+     *
+     * @param tempPrev - пересозданный лист из листа начального состояния.
+     * @param current  - лист с текущим состоянием
+     * @return - оставшиеся элементы из врменнего листа содержавший лист с текущим состоянием
+     */
+    private List<User> infoChanged(List<User> tempPrev, List<User> current) {
+        List<User> tempCurt = new ArrayList<>(current);
+        for (User userP : tempPrev) {
+            for (int i = 0; i < tempCurt.size(); i++) {
+                if (userP.getId().equals(tempCurt.get(i).getId()) && userP.getName().equals(tempCurt.get(i).getName())) {
+                    tempCurt.remove(i--);
+                } else if (userP.getId().equals(tempCurt.get(i).getId()) && !userP.getName().equals(tempCurt.get(i).getName())) {
+                    tempCurt.remove(i--);
+                    info.cnanged++;
+                }
+            }
+        }
+        return tempCurt;
+    }
+
+    /**
+     * Метод подсчета удаленных объектов.
+     * Работает за счет, того что удаляет из временного листа содержавший исходное состояние объекты которые отсуствуют
+     * в листе с текущим состоянием.
+     *
+     * @param previous - лист с изначальным состояние
+     * @param current  - лист с текущим состоянием
+     * @return - лист с оставшимися элеиз временного листа содержавший лист с исходным состоянием
+     */
+    private List<User> infoDeleted(List<User> previous, List<User> current) {
+        boolean check = false;
+        List<User> tempPrev = new ArrayList<>(previous);
+        for (int i = 0; i < tempPrev.size(); i++) {
+            for (User userC : current) {
+                if (tempPrev.get(i).getId().equals(userC.getId())) {
+                    check = true;
                     break;
                 }
             }
-            if (!this.check) {
-                info.;
+            if (!check) {
+                tempPrev.remove(i--);
+                this.info.deleted++;
             }
-            this.check = false;
+            check = false;
         }
-    }*/
+        return tempPrev;
+    }
 
 
     public static class User {
@@ -89,5 +109,17 @@ public class Analize {
         private int added;
         private int cnanged;
         private int deleted;
+
+        public int getAdded() {
+            return added;
+        }
+
+        public int getCnanged() {
+            return cnanged;
+        }
+
+        public int getDeleted() {
+            return deleted;
+        }
     }
 }
--- chapter_001/src/test/java/ru/job4j/task/AnalizeTest.java
+++ chapter_001/src/test/java/ru/job4j/task/AnalizeTest.java
@@ -6,8 +6,16 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 12.03.2019
+ */
+
 
 public class AnalizeTest {
     Analize.User user1 = new Analize.User(111, "Denis");
@@ -29,7 +37,17 @@ public class AnalizeTest {
     }
 
     @Test
-    public void test() {
+    public void whenIfExistDoubleUserInCurrentList() {
+        previous.add(user1);
+        previous.add(user4);
+        current.add(user2);
+        current.add(user2);
+        current.add(user3);
+        assertThat(new Analize().diff(previous, current).getAdded(), is(2));
+    }
+
+    @Test
+    public void whenAddNewThreeUserAndOndeChangedAndOneDeleted() {
         previous.add(user1);
         previous.add(user4);
         previous.add(user7);
@@ -41,22 +59,29 @@ public class AnalizeTest {
         current.add(user6);
         Analize analize = new Analize();
         Analize.Info info = analize.diff(previous, current);
-        //List<Analize.User> add = info.getListAdded();
-
-        int changed = 0;
+        assertThat(info.getAdded(), is(3));
+        assertThat(info.getCnanged(), is(1));
+        assertThat(info.getDeleted(), is(1));
     }
 
     @Test
     public void whenAddNewThreeUser() {
         previous.add(user1);
         previous.add(user4);
-        current.add(user1);
+        previous.add(user7);
         current.add(user2);
         current.add(user3);
-        current.add(user4);
         current.add(user5);
-        Analize analize = new Analize();
-        Analize.Info info = analize.diff(previous, current);
-        //assertThat(info.getListAdded().size(), is(3));
+        assertThat(new Analize().diff(previous, current).getAdded(), is(3));
     }
+
+    @Test
+    public void whenChangedOneUser() {
+        previous.add(user1);
+        previous.add(user4);
+        current.add(user6);
+        assertThat(new Analize().diff(previous, current).getCnanged(), is(1));
+
+    }
+
 }
