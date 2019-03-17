b67db2cdfad4790d711e4e60869527ba7ef56311
anderson178 <denismironenko@yandex.ru>
2019-03-17 16:24:12 +0300

    Stepik - symmetricDifference. The Set collection

---
+++ stepik/src/main/java/ru/job4j/set/Difference.java
@@ -0,0 +1,31 @@
+package ru.job4j.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Difference {
+    /**
+     * Метод проверяет разницу структур, разницу записывает в новую структуру
+     * @param set1 -  первая структура
+     * @param set2 - вторая структура
+     * @param <T> - тип данных
+     * @return - структура содержащая разницу двух входящих структур
+     */
+    public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
+        boolean check = false;
+        Set<T> rst = new HashSet<T>();
+
+        for(T object : set1) {
+            if (!set2.contains(object)) {
+                rst.add(object);
+            }
+        }
+        for(T object : set2) {
+            if (!set1.contains(object)) {
+                rst.add(object);
+            }
+        }
+
+        return rst;
+    }
+}
---
+++ stepik/src/test/java/ru/job4j/set/DifferenceTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class DifferenceTest {
+    @Test
+    public void whenDifferenceseroAndThree() {
+        Set<Integer> set1 = new HashSet<>(Set.of(1, 2, 3));
+        Set<Integer> set2 = new HashSet<>(Set.of(0, 1, 2));
+        Set<Integer> expected = new HashSet<>(Set.of(0, 3));
+        assertThat(Difference.symmetricDifference(set1,set2), is(expected));
+    }
+}
