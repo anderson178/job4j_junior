7868576e10ef7aea3405a95ab897e59a9d0ec2be
anderson178 <denismironenko@yandex.ru>
2019-03-20 17:01:55 +0300

    oral task for Petr

---
+++ chapter_001/src/main/java/ru/job4j/task/Contains.java
@@ -0,0 +1,36 @@
+package ru.job4j.task;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 20.03.2019
+ */
+
+public class Contains {
+
+    /**
+     * Метод проверяет содержатся ли символы одной коллекции в другой
+     *
+     * @param listOne - первая коллекция
+     * @param listTwo - вторая коллекция
+     * @return - true/false
+     */
+    public boolean checkContains(List<Integer> listOne, List<Integer> listTwo) {
+        boolean rst = true;
+        Map<Integer, Integer> map = new HashMap<>();
+        listTwo.forEach(o -> map.put(o, null));
+        for (Integer value : listOne) {
+            if (!map.containsKey(value)) {
+                rst = false;
+                break;
+            }
+        }
+        return rst;
+
+
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/task/ContainsTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.task;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 20.03.2019
+ */
+
+public class ContainsTest {
+
+    @Test
+    public void whenCheckContainsEqualseTrue() {
+        List<Integer> listOne = new ArrayList<>(Arrays.asList(1,2,3,4));
+        List<Integer> listTwo = new ArrayList<>(Arrays.asList(4,3,2,1));
+        assertThat(new Contains().checkContains(listOne,listTwo), is(true));
+    }
+    @Test
+    public void whenCheckContainsEqualseFalse() {
+        List<Integer> listOne = new ArrayList<>(Arrays.asList(1,2,3,4));
+        List<Integer> listTwo = new ArrayList<>(Arrays.asList(4,3,2,7));
+        assertThat(new Contains().checkContains(listOne,listTwo), is(false));
+    }
+}
