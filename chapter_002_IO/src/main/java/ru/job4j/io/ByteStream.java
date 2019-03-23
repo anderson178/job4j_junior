d76f82af53228c9716af3112edc43a3e01063882
anderson178 <denismironenko@yandex.ru>
2019-03-24 01:17:32 +0300

    IO. Check byte stream

--- chapter_002_IO/src/main/java/ru/job4j/io/ByteStream.java
+++ chapter_002_IO/src/main/java/ru/job4j/io/ByteStream.java
@@ -1,20 +1,26 @@
 package ru.job4j.io;
 
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStream;
 
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 24.03.2019
+ */
 public class ByteStream {
-    public int p;
-    public String k;
 
-    public boolean check(List<Integer> list1, List<Integer> list2) {
-        return false;
+    /**
+     * Метод проверяет на четность число находящееся в байтовом потоке
+     *
+     * @param in - входящий байтовый поток
+     * @return - четное/нечетное true/false
+     * @throws IOException
+     */
+    public boolean isNumber(InputStream in) throws IOException {
+        byte[] array = in.readAllBytes();
+        return array.length > 0 && array[array.length - 1] % 2 == 0;
     }
-
-    public static void main(String[] args) {
-        int p;
-
-    }
-
 
 
 }
--- chapter_002_IO/src/test/java/ru/job4j/io/ByteStreamTest.java
+++ chapter_002_IO/src/test/java/ru/job4j/io/ByteStreamTest.java
@@ -1,4 +1,42 @@
 package ru.job4j.io;
 
+import org.junit.Test;
+
+import java.io.*;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 24.03.2019
+ */
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class ByteStreamTest {
+
+    @Test
+    public void whenIsNumberFalse() {
+        ByteStream bs = new ByteStream();
+        try (InputStream in = new ByteArrayInputStream(new FileInputStream(
+                "C://projects//job4j_junior//chapter_002_IO//src//main//resources//byteFiles//67.txt")
+                .readAllBytes())) {
+            assertThat(bs.isNumber(in), is(false));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void whenIsNumberTrue() {
+        ByteStream bs = new ByteStream();
+        try (InputStream in = new ByteArrayInputStream(new FileInputStream(
+                "C://projects//job4j_junior//chapter_002_IO//src//main//resources//byteFiles//10.txt")
+                .readAllBytes())) {
+            assertThat(bs.isNumber(in), is(true));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
 }
