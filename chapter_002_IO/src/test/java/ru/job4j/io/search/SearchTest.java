c279be55ca5b06727180179bbc4c53958e3e2266
anderson178 <denismironenko@yandex.ru>
2019-04-01 15:25:53 +0300

    IO. File system scan - refactoring

--- chapter_002_IO/src/test/java/ru/job4j/io/search/SearchTest.java
+++ chapter_002_IO/src/test/java/ru/job4j/io/search/SearchTest.java
@@ -1,6 +1,6 @@
 package ru.job4j.io.search;
 
-import org.junit.Before;
+
 import org.junit.Test;
 
 import java.io.File;
@@ -15,40 +15,66 @@ import static org.hamcrest.Matchers.is;
 /**
  * @author Денис Мироненко
  * @version $Id$
- * @since 25.03.2019
+ * @since 02.04.2019
  */
 
 public class SearchTest {
-    private String path = System.getProperty("java.io.tmpdir") + "chapter_002_IO";
-    private File root = new File(path);
-    private File inRoot = new File(root.getPath());
-    private File fileOne = new File(root, "123.jpg");
-    private File fileTwo = new File(root, "hello.txt");
-    private File fileThree = new File(root, "photo.jpg");
-    private File fileFour = new File(inRoot, "table.xlsx");
+    private static final String SR = File.separator;
 
-    @Before
-    public void setUp() throws IOException {
+    @Test
+    public void whenTwoFilesExtentionJpg() throws IOException {
+        String path = System.getProperty("java.io.tmpdir") + "chapter_002_IO" + SR;
+        Search.clearDir(path);
+        File root = new File(path);
+        File inRoot = new File(root.getPath());
+        File fileOne = new File(root, "123.jpg");
+        File fileTwo = new File(root, "hello.txt");
+        File fileThree = new File(root, "photo.jpg");
+        File fileFour = new File(inRoot, "table.xlsx");
         root.mkdir();
         inRoot.mkdir();
         fileOne.createNewFile();
         fileTwo.createNewFile();
         fileThree.createNewFile();
         fileFour.createNewFile();
-    }
-
-    @Test
-    public void whenTwoFilesExtentionJpg() {
         assertThat(new Search().files(path, new String[]{"jpg"}), is(new ArrayList<>(Arrays.asList(fileOne, fileThree))));
     }
 
     @Test
-    public void whenOneFileExtentionXlsx() {
+    public void whenOneFileExtentionXlsx() throws IOException {
+        String path = System.getProperty("java.io.tmpdir") + "chapter_002_IO" + SR;
+        Search.clearDir(path);
+        File root = new File(path);
+        File inRoot = new File(root.getPath());
+        File fileOne = new File(root, "123.jpg");
+        File fileTwo = new File(root, "hello.txt");
+        File fileThree = new File(root, "photo.jpg");
+        File fileFour = new File(inRoot, "table.xlsx");
+        root.mkdir();
+        inRoot.mkdir();
+        fileOne.createNewFile();
+        fileTwo.createNewFile();
+        fileThree.createNewFile();
+        fileFour.createNewFile();
         assertThat(new Search().files(path, new String[]{"xlsx"}), is(new ArrayList<>(Arrays.asList(fileFour))));
     }
 
     @Test
-    public void whenThreeFilesExtentionJpgAndTxt() {
+    public void whenThreeFilesExtentionJpgAndTxt() throws IOException {
+        String path = System.getProperty("java.io.tmpdir") + "chapter_002_IO" + SR;
+        Search.clearDir(path);
+        File root = new File(path);
+        File inRoot = new File(root.getPath());
+        File fileOne = new File(root, "123.jpg");
+        File fileTwo = new File(root, "hello.txt");
+        File fileThree = new File(root, "photo.jpg");
+        File fileFour = new File(inRoot, "table.xlsx");
+        root.mkdir();
+        inRoot.mkdir();
+        fileOne.createNewFile();
+        fileTwo.createNewFile();
+        fileThree.createNewFile();
+        fileFour.createNewFile();
         assertThat(new Search().files(path, new String[]{"jpg", "txt"}), is(
                 new ArrayList<>(Arrays.asList(fileOne, fileTwo, fileThree))));
     }
