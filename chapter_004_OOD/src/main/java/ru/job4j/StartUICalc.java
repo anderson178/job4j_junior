5df6136efc22419643091406d35b643a87353721
anderson178 <denismironenko@yandex.ru>
2019-09-08 20:39:30 +0300

    task_calc_ocp
    # Conflicts:
    #	chapter_004_OOD/src/main/java/ru/job4j/StartUICalc.java
    #	chapter_004_OOD/src/main/java/ru/job4j/ocp/engineerCalc/MenuEngineer.java
    #	chapter_004_OOD/src/main/java/ru/job4j/srp/ordinaryCalc/MenuCalculator.java

---
+++ chapter_004_OOD/src/main/java/ru/job4j/StartUICalc.java
@@ -0,0 +1,40 @@
+package ru.job4j;
+
+import ru.job4j.ocp.engineer.MenuEngineer;
+import ru.job4j.srp.ordinary.ConsoleInput;
+import ru.job4j.srp.ordinary.Input;
+import ru.job4j.srp.ordinary.MenuCalculator;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 14.08.2019
+ */
+public class StartUICalc {
+    private MenuCalculator menu;
+    private Input input;
+    public Double result = 0.0;
+    public boolean work = true;
+
+    public StartUICalc(Input input, MenuCalculator menu) {
+        this.input = input;
+        this.menu = menu;
+    }
+
+    public void execute() {
+        menu.fillAction();
+        menu.fillDefaultActions(this);
+        do {
+            menu.show();
+            int key = input.ask("select: ", menu.fillRange());
+            this.result = menu.select(key, input, this);
+            if (key != menu.getSize() - 1 && key != menu.getSize() - 2) {
+                System.out.println(result);
+            }
+        } while (work);
+    }
+
+    public static void main(String[] args) {
+        new StartUICalc(new ConsoleInput(), new MenuEngineer()).execute();
+    }
+}
