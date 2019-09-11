5cf6c6d072746140601f67b0bf7238fd7bb8c82e
anderson178 <denismironenko@yandex.ru>
2019-09-11 11:12:21 +0300

    Subsequence

--- stepik/src/main/java/ru/job4j/tsystems/Subsequence.java
+++ stepik/src/main/java/ru/job4j/tsystems/Subsequence.java
@@ -8,32 +8,41 @@ public class Subsequence {
 
     public void find(List<String> first, List<String> second) {
         List<String> rst = new ArrayList<>();
-        for(String symbolFirst : first) {
+        for (String symbolFirst : first) {
             char[] masSymbolsFirst = symbolFirst.toCharArray();
             for (String symbolSecond : second) {
                 char[] masSymbolsSecond = symbolSecond.toCharArray();
 
-
-
-                if (masSymbolsFirst.length == masSymbolsSecond.length) {
-                    int count =0;
-                    for (int i = 0; i < masSymbolsFirst.length; i++) {
-                        if (masSymbolsFirst[i] == masSymbolsSecond[i]) {
-                            count++;
-                        } else {
-                            break;
-                        }
-                    }
-                    if (count == masSymbolsFirst.length) {
-                        rst.add(symbolSecond);
-                    }
-
+                if (compare(masSymbolsFirst, masSymbolsSecond)) {
+                    rst.add(symbolSecond);
+                } else {
+                    break;
                 }
+
 
             }
         }
-        int p =0;
+        int p = 0;
 
+    }
+
+    private boolean compare(char[] masSymbolsFirst, char[] masSymbolsSecond) {
+        boolean rst = true;
+        if (masSymbolsFirst.length == masSymbolsSecond.length) {
+            int count = 0;
+            for (int i = 0; i < masSymbolsFirst.length; i++) {
+                if (masSymbolsFirst[i] == masSymbolsSecond[i]) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            if (count != masSymbolsFirst.length) {
+                rst = false;
+            }
+
+        }
+        return rst;
     }
 
 
