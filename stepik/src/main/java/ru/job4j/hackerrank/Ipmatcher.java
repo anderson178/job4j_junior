10a5edcfc90535c0151bc2065b220bd42d89dec2
anderson178 <denismironenko@yandex.ru>
2019-09-17 14:22:21 +0300

    hackerrank - ip RegEx

---
+++ stepik/src/main/java/ru/job4j/hackerrank/Ipmatcher.java
@@ -0,0 +1,18 @@
+package ru.job4j.hackerrank;
+
+import java.util.Scanner;
+
+public class Ipmatcher {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+    }
+}
+class MyRegex {
+   public final String pattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+}
